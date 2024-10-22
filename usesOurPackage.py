0e15c967cf11be42d827412ce4c2f9a6a9324c02
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-22 06:14:21 -0400

    Add files via upload

---
+++ usesOurPackage.py
@@ -0,0 +1,18 @@
+# usesOurPackage.py
+
+from mylib import multiply, divide
+
+multiplied = multiply(4, 4)
+print(multiplied)
+
+divided = divide(10, 2)
+print(divided)
+
+####
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
